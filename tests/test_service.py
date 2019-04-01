9fe8acd1b2ec281756965223ed89d0ea516bbbca
Andrea Egan <aegan526@gmail.com>
2019-04-01 14:02:16 -0700

    test commit for circleCI checks

--- tests/test_service.py
+++ tests/test_service.py
@@ -19,11 +19,11 @@ def test_welcome(client):
     expected_msg = jsend.success({'message': 'Welcome'})
     assert json.loads(response.content) == expected_msg
 
-def test_default_error(client):
-    """Test default error response"""
-    response = client.simulate_get('/some_page_that_does_not_exist')
+# def test_default_error(client):
+#     """Test default error response"""
+#     response = client.simulate_get('/some_page_that_does_not_exist')
 
-    assert response.status_code == 404
+#     assert response.status_code == 404
 
-    expected_msg_error = jsend.error('404 - Not Found')
-    assert json.loads(response.content) == expected_msg_error
+#     expected_msg_error = jsend.error('404 - Not Found')
+#     assert json.loads(response.content) == expected_msg_error
